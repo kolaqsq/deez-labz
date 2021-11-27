114a812c6f64112b8e05bc50f1de53b3775c140d
Nikolay Ogorodnikov <kolaqsq1707@gmail.com>
2021-11-27 18:33:49 +0300

    adapter finish

--- app/src/main/java/com/example/mob5/Adapter.kt
+++ app/src/main/java/com/example/mob5/Adapter.kt
@@ -1,4 +1,44 @@
+/*
+Адаптер — это структурный паттерн проектирования,
+который позволяет объектам с несовместимыми
+интерфейсами работать вместе.
+*/
+
 package com.example.mob5
 
-class Adapter {
+class MarcoV1(encoding: String, separator: Char){
+    private val encode = encoding
+    private val separate = separator
+
+    fun getEncoding():String{
+        return encode
+    }
+    fun getSeparator():Char{
+        return separate
+    }
+}
+
+class MarcoV2(Config: MutableList<Any>){
+    private val conf = Config
+
+    fun getConfigs():MutableList<Any>{
+        return conf
+    }
+}
+
+class AdapterV1V2(macroV1: MarcoV1) {
+    private val v1 = macroV1
+
+    fun getConfig():MutableList<Any>{
+        return mutableListOf(v1.getEncoding(), v1.getSeparator())
+    }
+}
+
+fun main(){
+    val myConfigOnV1 = MarcoV1("unicode-32", ';')
+
+    val adapter = AdapterV1V2(myConfigOnV1)
+
+    val myConfigOnV2 = MarcoV2(adapter.getConfig())
+    println(myConfigOnV2.getConfigs())
 }
