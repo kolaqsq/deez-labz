6e480dbd7a03a641455c0beab943bca6bf67bef1
Nikolay Ogorodnikov <kolaqsq1707@gmail.com>
2021-12-11 22:35:23 +0300

    added numpads for third fragment

--- app/src/main/java/com/example/hw3/ThirdFragment.kt
+++ app/src/main/java/com/example/hw3/ThirdFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.hw3.databinding.FragmentThirdBinding
@@ -26,20 +25,29 @@ class ThirdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Добавляем тригерр на изменения editText
-        // Передаем данные через ViewModels
-        binding.textInput.doAfterTextChanged {
-            dataModel.operation.value = binding.textInput.text.toString()
-        }
-        // Добавляем тригерр для перехода на следующий fragment
-        binding.buttonNext.setOnClickListener {
+
+        // Функция для выбора оператора и переходна на следующий экран
+        fun select(operator: String) {
+            dataModel.operation.value = operator
             val activityCallBack = requireActivity() as ActivityCallBack
             activityCallBack.next()
         }
-        // Добавляем тригерр для перехода на предыдущий fragment
-        binding.buttonPrev.setOnClickListener {
-            val activityCallBack = requireActivity() as ActivityCallBack
-            activityCallBack.prev()
+
+        // Добавляем триггеры для кнопок выбора операторов
+        binding.inputAddition.setOnClickListener {
+            select("+")
+        }
+
+        binding.inputSubtraction.setOnClickListener {
+            select("-")
+        }
+
+        binding.inputDivision.setOnClickListener {
+            select("/")
+        }
+
+        binding.inputMultiplication.setOnClickListener {
+            select("*")
         }
     }
 }
